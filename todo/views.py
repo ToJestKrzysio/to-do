e83fb3f5ce8aba2fbd2f55f44e20ce6f20cea9ab
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-11-26 22:07:33 +0100

    Display only user tasks

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse_lazy, reverse, resolve
 from django.views import View
 from django.views.generic import CreateView
 from todo.models import Task
@@ -15,15 +15,17 @@ class TaskView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        user = self.request.user
         match self.kwargs.get("filter", None):
             case "all":
-                context['task_list'] = Task.objects.filter(archive=False)
+                context['task_list'] = Task.objects.filter(
+                    archive=False, owner_id=user.id)
             case "done":
                 context['task_list'] = Task.objects.filter(
-                    is_done=True, archive=False)
+                    is_done=True, archive=False, owner_id=user.id)
             case _:
                 context['task_list'] = Task.objects.filter(
-                    is_done=False, archive=False)
+                    is_done=False, archive=False, owner_id=user.id)
         return context
 
     def form_valid(self, form):
@@ -35,8 +37,9 @@ class ArchiveTaskView(View):
 
     def post(self, request, pk):
         task = Task.objects.get(pk=pk)
-        task.archive = True
-        task.save()
+        if task.owner == request.user or request.user.is_superuser:
+            task.archive = True
+            task.save()
         return HttpResponseRedirect(reverse('todo:todo'))
 
 
@@ -44,6 +47,7 @@ class DoneTaskView(View):
 
     def post(self, request, pk):
         task = Task.objects.get(pk=pk)
-        task.is_done = True
-        task.save()
+        if task.owner == request.user or request.user.is_superuser:
+            task.is_done = True
+            task.save()
         return HttpResponseRedirect(reverse('todo:todo'))
