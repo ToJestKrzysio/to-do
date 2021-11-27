c9caa16bb1d7702c895dab3d1ddff2f7dde68ac2
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-11-27 14:21:54 +0100

    hotfix for views

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms import SplitDateTimeWidget
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.views import View
