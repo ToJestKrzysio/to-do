da4c9130457b685534236aa1ee3f4c17d8faf021
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-11-29 12:51:15 +0100

    TD-013 Tests for ArchiveTaskView

--- todo/tests/conftest.py
+++ todo/tests/conftest.py
@@ -6,16 +6,35 @@ from todo.models import Task
 
 
 @pytest.fixture
-def new_user(client, django_user_model):
+def user(client, django_user_model):
     username = "username_1"
     password = "NotVeryStrongPassword123"
     user = django_user_model.objects.create_user(username=username,
                                                  password=password)
     return user
 
+
 @pytest.fixture
-def user_with_task(client, django_user_model, new_user):
-    Task.objects.create(is_done=False, archive=False, details="details",
+def user_2(client, django_user_model):
+    username = "username_2"
+    password = "NotVeryStrongPassword456"
+    user = django_user_model.objects.create_user(username=username,
+                                                 password=password)
+    return user
+
+
+@pytest.fixture
+def user_task(client, django_user_model, user):
+    task = Task.objects.create(is_done=False, archive=False, details="details",
                         title="Test", deadline=timezone.now(),
-                        owner_id=new_user.id, pk=1)
-    return new_user
+                        owner_id=user.id, pk=1)
+    return task
+
+
+@pytest.fixture
+def superuser(client, django_user_model):
+    username = "TheSuperuser"
+    password = "NotVeryStrongPassword789"
+    user = django_user_model.objects.create_superuser(username=username,
+                                                      password=password)
+    return user
--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -1,25 +1,91 @@
 import pytest
 from django.urls import reverse
 
+from todo.models import Task
 
+
+@pytest.mark.django_db
 class TestArchiveTaskView:
 
     @pytest.mark.parametrize("url", [
         "/todo/delete/1/all",
         reverse("todo:delete", kwargs={"pk": 1, "selection": "all"}),
     ])
-    def test_response_code_for_unauthenticated_user(self, client, url):
+    def test_for_unauthenticated_user(self, client, url, user, user_task):
+        """
+        Test if unauthenticated user cannot archive any task.
+        User should be redirected to login page,
+        selected task should be unaffected.
+        """
+        assert len(Task.objects.filter(archive=False)) == 1
+
         response = client.post(url)
+
         assert response.status_code == 302
         assert "accounts/login/?next=/todo/delete/" in response.url
+        assert len(Task.objects.filter(archive=False)) == 1
 
     @pytest.mark.parametrize("url", [
         "/todo/delete/1/all",
         reverse("todo:delete", kwargs={"pk": 1, "selection": "all"}),
     ])
-    def test_response_code_for_authenticated_user(self, client, user_with_task,
-                                                  url):
-        client.force_login(user_with_task)
+    def test_for_authenticated_user(self, client, url, user, user_task):
+        """
+        Test if authenticated user who is owner of a task can archive it.
+        User should be redirected to filter view with appropriate selection,
+        selected task should be marked as archived.
+        """
+        assert len(Task.objects.filter(archive=True)) == 0
+        assert len(Task.objects.filter(archive=False)) == 1
+
+        client.force_login(user)
         response = client.post(url)
+
         assert response.status_code == 302
         assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(archive=True)) == 1
+        assert len(Task.objects.filter(archive=False)) == 0
+
+    @pytest.mark.parametrize("url", [
+        "/todo/delete/1/all",
+        reverse("todo:delete", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_superuser(self, client, url, superuser, user_task):
+        """
+        Tests if superuser can archive other user task.
+        User should be redirected to filter view with appropriate selection,
+        selected task should be marked as archived.
+        """
+        assert len(Task.objects.filter(archive=True)) == 0
+        assert len(Task.objects.filter(archive=False)) == 1
+
+        client.force_login(superuser)
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(archive=True)) == 1
+        assert len(Task.objects.filter(archive=False)) == 0
+
+    @pytest.mark.parametrize("url", [
+        "/todo/delete/1/all",
+        reverse("todo:delete", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_user_with_foreign_taks(
+            self, client, url, user_2, user_task
+    ):
+        """
+        Tests if user can archive other user task.
+        User should be redirected to filter view with appropriate selection,
+        selected task should not be marked as archived.
+        """
+        assert len(Task.objects.filter(archive=True)) == 0
+        assert len(Task.objects.filter(archive=False)) == 1
+
+        client.force_login(user_2)
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(archive=True)) == 0
+        assert len(Task.objects.filter(archive=False)) == 1
