d18c12a87fa72a2e587f3fb2ecd223eb1e3988aa
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-11-29 13:08:31 +0100

    TD-013 TestDoneTaskView

--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -15,7 +15,7 @@ class TestArchiveTaskView:
         """
         Test if unauthenticated user cannot archive any task.
         User should be redirected to login page,
-        selected task should be unaffected.
+        selected task archive field should be unaffected.
         """
         assert len(Task.objects.filter(archive=False)) == 1
 
@@ -33,7 +33,7 @@ class TestArchiveTaskView:
         """
         Test if authenticated user who is owner of a task can archive it.
         User should be redirected to filter view with appropriate selection,
-        selected task should be marked as archived.
+        selected task archived field should be set to True.
         """
         assert len(Task.objects.filter(archive=True)) == 0
         assert len(Task.objects.filter(archive=False)) == 1
@@ -54,7 +54,7 @@ class TestArchiveTaskView:
         """
         Tests if superuser can archive other user task.
         User should be redirected to filter view with appropriate selection,
-        selected task should be marked as archived.
+        selected task archived field should be set to True.
         """
         assert len(Task.objects.filter(archive=True)) == 0
         assert len(Task.objects.filter(archive=False)) == 1
@@ -71,13 +71,11 @@ class TestArchiveTaskView:
         "/todo/delete/1/all",
         reverse("todo:delete", kwargs={"pk": 1, "selection": "all"}),
     ])
-    def test_for_user_with_foreign_taks(
-            self, client, url, user_2, user_task
-    ):
+    def test_for_user_with_foreign_taks(self, client, url, user_2, user_task):
         """
         Tests if user can archive other user task.
         User should be redirected to filter view with appropriate selection,
-        selected task should not be marked as archived.
+        selected task archive field should be unaffected.
         """
         assert len(Task.objects.filter(archive=True)) == 0
         assert len(Task.objects.filter(archive=False)) == 1
@@ -89,3 +87,88 @@ class TestArchiveTaskView:
         assert "/todo/filter/all" in response.url
         assert len(Task.objects.filter(archive=True)) == 0
         assert len(Task.objects.filter(archive=False)) == 1
+
+
+@pytest.mark.django_db
+class TestDoneTaskView:
+
+    @pytest.mark.parametrize("url", [
+        "/todo/done/1/all",
+        reverse("todo:done", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_unauthenticated_user(self, client, url, user, user_task):
+        """
+        Test if unauthenticated user cannot complete any task.
+        User should be redirected to login page,
+        selected task is_done field should be unaffected.
+        """
+        assert len(Task.objects.filter(is_done=False)) == 1
+
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "accounts/login/?next=/todo/done/" in response.url
+        assert len(Task.objects.filter(is_done=False)) == 1
+
+    @pytest.mark.parametrize("url", [
+        "/todo/done/1/all",
+        reverse("todo:done", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_authenticated_user(self, client, url, user, user_task):
+        """
+        Test if authenticated user who is owner of a task can complete it.
+        User should be redirected to filter view with appropriate selection,
+        selected task is_done field should be set to True.
+        """
+        assert len(Task.objects.filter(is_done=True)) == 0
+        assert len(Task.objects.filter(is_done=False)) == 1
+
+        client.force_login(user)
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(is_done=True)) == 1
+        assert len(Task.objects.filter(is_done=False)) == 0
+
+    @pytest.mark.parametrize("url", [
+        "/todo/done/1/all",
+        reverse("todo:done", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_superuser(self, client, url, superuser, user_task):
+        """
+        Tests if superuser can complete other user task.
+        User should be redirected to filter view with appropriate selection,
+        selected task is_done field should be set to True.
+        """
+        assert len(Task.objects.filter(is_done=True)) == 0
+        assert len(Task.objects.filter(is_done=False)) == 1
+
+        client.force_login(superuser)
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(is_done=True)) == 1
+        assert len(Task.objects.filter(is_done=False)) == 0
+
+    @pytest.mark.parametrize("url", [
+        "/todo/done/1/all",
+        reverse("todo:done", kwargs={"pk": 1, "selection": "all"}),
+    ])
+    def test_for_user_with_foreign_taks(self, client, url, user_2, user_task):
+        """
+        Tests if user can complete other user task.
+        User should be redirected to filter view with appropriate selection,
+        selected task is_done field should be unaffected.
+        """
+        assert len(Task.objects.filter(is_done=True)) == 0
+        assert len(Task.objects.filter(is_done=False)) == 1
+
+        client.force_login(user_2)
+        response = client.post(url)
+
+        assert response.status_code == 302
+        assert "/todo/filter/all" in response.url
+        assert len(Task.objects.filter(is_done=True)) == 0
+        assert len(Task.objects.filter(is_done=False)) == 1
